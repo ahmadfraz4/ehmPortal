e74a6824d8cb4232b2995c0b91769c8af2545b56
Ahmad Faraz <ahmadfraz2015@gmail.com>
2025-10-19 04:26:54 +0500

    done

--- resources/views/components/group-chat-btn.blade.php
+++ resources/views/components/group-chat-btn.blade.php
@@ -20,7 +20,7 @@
     <div id="groupDropdown-{{ $item->id }}"
         class="absolute right-4 top-12 hidden z-[9999] bg-white divide-y divide-gray-100 rounded-lg shadow-lg w-44">
         <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownDefaultButton">
-            <li><a href="#" onclick="if (confirm('Do you want to leave Channel?')) leaveChannel({{ $item->id }})" class="block px-4 py-2 hover:bg-gray-100">Leave Channel</a></li>
+            <li><a href="#" onclick="if (confirm('Do you want to leave Group?')) leaveChannel({{ $item->id }})" class="block px-4 py-2 hover:bg-gray-100">Leave Group</a></li>
         </ul>
     </div>
 </div>
